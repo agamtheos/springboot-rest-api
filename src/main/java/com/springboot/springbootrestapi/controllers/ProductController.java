6d17f0f600b0dff8f6b4da515f54942427dac76d
agamtheos <agamtheos@gmail.com>
2022-12-02 19:47:31 +0700

    remove unnecessary import

--- src/main/java/com/springboot/springbootrestapi/controllers/ProductController.java
+++ src/main/java/com/springboot/springbootrestapi/controllers/ProductController.java
@@ -1,6 +1,5 @@
 package com.springboot.springbootrestapi.controllers;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
