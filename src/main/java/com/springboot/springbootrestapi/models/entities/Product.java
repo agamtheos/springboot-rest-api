68577de5fa4648f6757a25a9e3728798b8db0249
agamtheos <agamtheos@gmail.com>
2022-12-02 22:24:47 +0700

    add validation, relations table

--- src/main/java/com/springboot/springbootrestapi/models/entities/Product.java
+++ src/main/java/com/springboot/springbootrestapi/models/entities/Product.java
@@ -1,13 +1,19 @@
 package com.springboot.springbootrestapi.models.entities;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "tbl_products")
@@ -19,12 +25,26 @@ public class Product implements Serializable{
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @NotEmpty(message = "Name is required")
   @Column(name="product_name", length = 100, nullable = false)
   private String name;
 
+  @NotEmpty(message = "Description is required")
   private String description;
 
   private double price;
+
+  @ManyToOne
+  private Category category;
+
+  @ManyToMany
+  @JoinTable(
+    name = "tbl_product_suppliers",
+    joinColumns = @JoinColumn(name = "product_id",
+    referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "supplier_id"
+    ))
+  private Set<Supplier> suppliers;
 
   public Product() {
 
@@ -69,5 +89,25 @@ public class Product implements Serializable{
     this.price = price;
   }
 
+  public Category getCategory() {
+    return category;
+  }
 
+  public void setCategory(Category category) {
+    this.category = category;
+  }
+
+  public static long getSerialversionuid() {
+    return serialVersionUID;
+  }
+
+  public Set<Supplier> getSuppliers() {
+    return suppliers;
+  }
+
+  public void setSuppliers(Set<Supplier> suppliers) {
+    this.suppliers = suppliers;
+  }
+
+  
 }
